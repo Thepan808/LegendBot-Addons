d0837afde1fd9bf165edeb26991e97e1d4529c11
Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-10-22 20:18:34 +0530

    Update animations6.py

--- animations6.py
+++ animations6.py
@@ -394,6 +394,53 @@ async def _(event):
         await asyncio.sleep(animation_interval)
         await event.edit(animation_chars[i % 82])
 
+@borg.on(admin_cmd(pattern=f"sadmin", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 1
+    animation_ttl = range(0, 13)
+    await event.edit("sadmin")
+    animation_chars = [
+
+
+
+            "@aaaaaaaaaaaaadddddddddddddmmmmmmmmmmmmmiiiiiiiiiiiiinnnnnnnnnnnnn",
+
+            "@aaaaaaaaaaaaddddddddddddmmmmmmmmmmmmiiiiiiiiiiiinnnnnnnnnnnn",    
+
+            "@aaaaaaaaaaadddddddddddmmmmmmmmmmmiiiiiiiiiiinnnnnnnnnnn",
+
+            "@aaaaaaaaaaddddddddddmmmmmmmmmmiiiiiiiiiinnnnnnnnnn",
+
+            "@aaaaaaaaadddddddddmmmmmmmmmiiiiiiiiinnnnnnnnn",
+
+            "@aaaaaaaaddddddddmmmmmmmmiiiiiiiinnnnnnnn",
+
+            "@aaaaaaadddddddmmmmmmmiiiiiiinnnnnnn",
+
+            "@aaaaaaddddddmmmmmmiiiiiinnnnnn",
+
+            "@aaaaadddddmmmmmiiiiinnnnn",    
+
+            "@aaaaddddmmmmiiiinnnn",
+
+            "@aaadddmmmiiinnn",
+
+            "@aaddmmiinn",
+
+            "@admin"
+
+        ]
+
+    for i in animation_ttl:
+            await asyncio.sleep(1)
+            await event.edit(animation_chars[i % 13])
+
+
+CmdHelp("shoutadmin").add_command(
+    'sadmin', None, 'υѕє αи∂ ѕєє'
+).add()
 CmdHelp("animations6").add_command(
   'bigoof', None, '🇮🇳🇮🇳🇮🇳'
 ).add_command(
@@ -420,8 +467,14 @@ CmdHelp("animations6").add_command(
   '^Tlol', None, 'Use and See'
 ).add_command(
   'chutiye', None, 'Animation Abuse'
+).add_command(
+  "sadmin", None, "Its Shout Admin"
+).add_info(
+  "Its all Animation Use As Anywhere"
+).add_warning(
+  "Harmless Module✅"
 ).add_type(
-  'Addons'
+  "Addons"
 ).add()
 
 #legendbot
