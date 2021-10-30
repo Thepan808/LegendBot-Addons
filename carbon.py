3de2fa65f2250c90a8bec5cf701c6550e9dcac23
Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-10-30 13:43:07 +0530

    Update carbon.py

--- carbon.py
+++ carbon.py
@@ -7,7 +7,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 from userbot import ALIVE_NAME, CMD_HELP
-from userbot.helpers.functions import deEmojify
+from userbot.helpers.funct import deEmojify
 from LEGENDBOT.utils import admin_cmd, edit_or_reply, sudo_cmd
 from userbot.cmdhelp import CmdHelp
 
